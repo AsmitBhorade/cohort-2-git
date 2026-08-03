1958d8d9cbb62f4267f1f995140478321da5187c
Asmit Bhorade <professional.aab2006@gmail.com>
2026-08-03 15:20:43 +0530

    QuerySelectorALL

--- script.js
+++ script.js
@@ -1,41 +1,12 @@
-const users = [
-    {
-        fullName: "Aarav Mehta",
-        profession: "UX Designer",
-        description: "Designs apps that feel human. Loves coffee & pixel-perfect layouts.",
-        tags: ["Figma", "UI/UX", "Design Systems", "Freelancer"]
-    },
-    {
-        fullName: "Emily Carter",
-        profession: "Software Engineer",
-        description: "Writes clean code and breaks things just to fix them better.",
-        tags: ["React", "TypeScript", "Open Source", "Tech Speaker"]
-    },
-    {
-        fullName: "Aisha Khan",
-        profession: "Data Scientist",
-        description: "Finds patterns in the noise. Speaks fluently in charts and numbers.",
-        tags: ["Python", "AI", "Machine Learning", "Analytics"]
-    },
-    {
-        fullName: "Noah Williams",
-        profession: "Content Creator",
-        description: "Makes complex topics easy. Lives on caffeine and camera angles.",
-        tags: ["Video Editing", "Education", "YouTube", "Storytelling"]
-    }
-];
+var allbtn= document.querySelectorAll('button')
 
-var sum = ''
+allbtn.forEach((ele)=>{
+    console.log('Hi')
 
-users.forEach(function (elem) {
-    sum = sum + `<div class="card">
-            <h2>${elem.fullName}</h2>
-            <h3>${elem.profession}</h3>
-        <h5>${elem.description}</h5>
-    </div> `
-
+    ele.addEventListener('click',()=>{
+        if(ele.innerHTML==='Follow')
+            ele.innerHTML='UnFollow'
+        else
+            ele.innerHTML='Follow'
+    })
 })
-
-var main = document.querySelector('main')
-
-main.innerHTML = sum
